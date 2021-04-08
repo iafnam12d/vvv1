3153456e0f4108f5ef25fb3078bd5e3237961d1a
iafnam12d <82164320+iafnam12d@users.noreply.github.com>
2021-04-08 20:45:15 +0300

    Add files via upload

---
+++ precache-manifest.d792811cb53471fa9ba9fac2cb126c77.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb83e926925e49a5fe18",
+    "url": "./static/css/main.710f5742.chunk.css"
+  },
+  {
+    "revision": "fb83e926925e49a5fe18",
+    "url": "./static/js/main.884683bd.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "476147095b34a7ca2d4a",
+    "url": "./static/css/2.a472d775.chunk.css"
+  },
+  {
+    "revision": "476147095b34a7ca2d4a",
+    "url": "./static/js/2.5a328858.chunk.js"
+  },
+  {
+    "revision": "c53e482c55280e18695f533a6fe47bee",
+    "url": "./static/media/coin.c53e482c.png"
+  },
+  {
+    "revision": "1808b5f6b2dd330757f13c6dc3ddf32a",
+    "url": "./static/media/1233.1808b5f6.ttf"
+  },
+  {
+    "revision": "d8dde2c45fabb1c7700b35203f77be7a",
+    "url": "./static/media/click.d8dde2c4.png"
+  },
+  {
+    "revision": "2ab76007cf601d8d216f5b5ef176821a",
+    "url": "./index.html"
+  }
+];
